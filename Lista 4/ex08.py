f6e4bfa3974815063bb2609f65cb1008abe2115b
AlineAlves10 <alves.aline1177@gmail.com>
2024-08-23 21:23:52 -0300

    ex

--- Lista 4/ex08.py
+++ Lista 4/ex08.py
@@ -1,2 +1,18 @@
 
+lista = []
 while True:
+    string = input()
+    if string == '.':
+      break
+    lista.append(string)
+
+lista.sort()
+
+tamanho = len(lista)
+meio = len(lista) // 2
+
+if tamanho % 2 == 0:
+    print(lista[meio - 1])  
+    print(lista[meio])      
+else:
+    print(lista[meio])
--- Lista 4/teste.py
+++ Lista 4/teste.py
@@ -1,30 +1,15 @@
-def slicing(palavra, num1, num2):
-    string = palavra
-    teste = string[num1:num2]
-    return teste
+nome = '0aline'
+nome_novo = ''
+nada = ''
+for i in nome:
+    if i == '0':
+        nada += i
+    else:
+        nome_novo += i
+    
+print(nome_novo)
 
-frase = input()
-fatia = input()
-com, fim = fatia.split()
-com = int(com)
-fim = int(fim)
-
-a = slicing(frase, com, fim)
-print(a)
-
-
-
-def slicing(palavra, num1, num2, num3):
-    string = palavra
-    teste = string[num1:num2:num3]
-    return teste
-
-frase = input()
-fatia = input()
-com, fim, passo = fatia.split()
-com = int(com)
-fim = int(fim)
-passo = int(passo)
-
-a = slicing(frase, com, fim, passo)
-print(a)
+nome = 'Profa.Maria'
+if nome.startswith('Profa. '): 
+   nome = nome.replace('Profa. ', '')
+print(nome)
